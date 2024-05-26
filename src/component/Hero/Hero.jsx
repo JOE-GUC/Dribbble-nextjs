965fea0bd5189b9e2cebe9be1fa8ce22900ba067
JOSEPH-JOSE <joeboy65458@agmail.com>
2024-05-26 18:52:02 +0100

    done

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Hero.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 // import './CourseImage.css'; // Import the CSS file
 
 const Hero = () => {
@@ -14,22 +15,165 @@ const Hero = () => {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
+
     return (
         <div className={styles.container}>
             <div>
-            <h5>Over 3 million ready-to-work creatives!</h5>
-            <h1>The world’s destination <br /> for design</h1>
-            <p>Get inspired by the work of millions of top-rated designers & agencies around the world.</p>
-            <button>Get Started</button>
+                <h5>Over 3 million ready-to-work creatives!</h5>
+                <h1>The world’s destination <br /> for design</h1>
+                <p>Get inspired by the work of millions of top-rated designers & agencies around the world.</p>
+                <button>Get Started</button>
             </div>
-            {/* Display fetched data */}
-            {data && data.map(course => (
-                <div key={course.id} className={styles.display}>
-                    <img src={course.media.image.small} alt="Course Image" />
+
+            <div className={styles.con}>
+                <div className={styles.cords}>
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.text}>bless</p>
+                            <span className={styles.links}>
+                                <p className={styles.joe + " " + styles.link}> Link</p>
+                                <p className={styles.joecolor}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.joe}>joseph</p>
+                            <span className={styles.links}>
+                                <p className={styles.joe + " " + styles.link}> Link</p>
+                                <p className={styles.joecolor}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.text}>edward</p>
+                            <span className={styles.links}>
+                                <p className={styles.text + " " + styles.link}> Link</p>
+                                <p className={styles.git}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image
+                            className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350}
+                        //      layout="fill"
+                        // objectFit="cover"
+                        />
+                        <span className={styles.info}>
+                            <p className={styles.text}>martin</p>
+                            <span className={styles.links}>
+                                <p className={styles.text + " " + styles.link}> Link</p>
+                                <p className={styles.git}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.text}>edward</p>
+                            <span className={styles.links}>
+                                <p className={styles.text + " " + styles.link}> Link</p>
+                                <p className={styles.git}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.joe}>joseph</p>
+                            <span className={styles.links}>
+                                <p className={styles.joe + " " + styles.link}> Link</p>
+                                <p className={styles.joecolor}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350} />
+                        <span className={styles.info}>
+                            <p className={styles.text}>bless</p>
+                            <span className={styles.links}>
+                                <p className={styles.joe + " " + styles.link}> Link</p>
+                                <p className={styles.joecolor}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                        <Image
+                            className={styles.img}
+                            src="/image.png"
+                            width={280}
+                            height={350}
+                        //      layout="fill"
+                        objectFit="cover"
+                        />
+                        <span className={styles.info}>
+                            <p className={styles.text}>martin</p>
+                            <span className={styles.links}>
+                                <p className={styles.text + " " + styles.link}> Link</p>
+                                <p className={styles.git}>
+                                    Git Hub
+                                </p>
+                            </span>
+                        </span>
+                    </Link>
                 </div>
-            ))}
-        </div>
+            </div>
+
+          {/* Display fetched data */}
+{data && data.map(course => (
+    <div key={course.id} className={styles.display}>
+        {course.media && course.media.image && course.media.image.small ? (
+            <img src={course.media.image.small} alt="Course Image" />
+        ) : (
+            <p>Error: Image URL not found</p>
+        )}
+    </div>
+))}
+        </div >
+    
+
     );
 };
+
 
 export default Hero;
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client'
-import React from 'react'
-import styles from './Navbar.module.css'
+import React from 'react';
+import styles from './Navbar.module.css';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
@@ -14,35 +14,34 @@ const Navbar = () => {
     { name: 'Sign up', href: '/register' }
   ];
 
-
-  <>
-  </>
-
   return (
-    <div className={styles.con}>
-      <img style={{ marginLeft: '20px', height: '85px',}} src="/dribbble-removebg-preview.png" alt="" />
-      {/* <h1 style={{ color: 'black', marginLeft: 20 }}> Dribbble</h1> */}
-      <p>Find designers</p>
-      <p>inspiration</p>
-      <p>Courses</p>
-      <p>Jobs</p>
-      <p>Go Pro</p>
-      <div className={styles.linksCon}>
-        <input type="search" name="search" id="" placeholder='Search' />
+    <>
+      <div className={styles.con}>
+        <img style={{ marginLeft: '20px', height: '85px' }} src="/dribbble-removebg-preview.png" alt="" />
+        <p>Find designers</p>
+        <p>Inspiration</p>
+        <p>Courses</p>
+        <p>Jobs</p>
+        <p>Go Pro</p>
 
-        {!session ? links.map((link, index) => (
-          <Link href={link.href} className={styles.link} key={index}>{link.name}</Link>
-        )) : (
-          map((link, index) => (
-            <Link href={link.href} className={styles.link} key={index}>{link.name}</Link>
-          )
-          )
-        )}{
-          session && <Image src={session.user.image} width={20} height={20} style={{ borderRadius: "50%" }} />
-        }
+        <div className={styles.linksCon}>
+          <input type="search" name="search" id="" placeholder='Search' />
+
+          {!session ? (
+            links.map((link, index) => (
+              <Link href={link.href} className={styles.link} key={index}>{link.name}</Link>
+            ))
+          ) : (
+            links.map((link, index) => (
+              <Link href={link.href} className={styles.link} key={index}>{link.name}</Link>
+            ))
+          )}
+
+          {session && <Image src={session.user.image} width={20} height={20} style={{ borderRadius: "50%" }} />}
+        </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
